79d9fb14792aba567dac43d8d11460710f6401ba
Kanai Lomayani <k.mahenge@gmail.com>
2025-12-04 04:34:34 +0300

    Invoice generation and sending testing 14

---
+++ AmbulanceRider.API/Migrations/20251204013424_PricingMatrixRegionRelation.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmbulanceRider.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class PricingMatrixRegionRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_pricing_matrices_Regions_RegionId",
+                table: "pricing_matrices");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_pricing_matrices_Regions_RegionId",
+                table: "pricing_matrices",
+                column: "RegionId",
+                principalTable: "Regions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_pricing_matrices_Regions_RegionId",
+                table: "pricing_matrices");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_pricing_matrices_Regions_RegionId",
+                table: "pricing_matrices",
+                column: "RegionId",
+                principalTable: "Regions",
+                principalColumn: "Id");
+        }
+    }
+}
