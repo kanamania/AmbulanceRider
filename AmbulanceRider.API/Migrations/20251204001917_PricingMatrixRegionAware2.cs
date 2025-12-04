235203c4f73f009d7ff405e6e916d68454089086
Kanai Lomayani <k.mahenge@gmail.com>
2025-12-04 03:19:47 +0300

    Invoice generation and sending testing 3

---
+++ AmbulanceRider.API/Migrations/20251204001917_PricingMatrixRegionAware2.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmbulanceRider.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class PricingMatrixRegionAware2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ContactEmail",
+                table: "companies",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TaxRate",
+                table: "companies",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TaxRate",
+                table: "companies");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ContactEmail",
+                table: "companies",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
